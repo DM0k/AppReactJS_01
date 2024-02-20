bec8259846689658867bfb3479612918b0649439
DM0k <129907524+DM0k@users.noreply.github.com>
2024-02-19 22:19:59 -0300

    Primeiro Comp Custom

--- app_01/src/pages/index.js
+++ app_01/src/pages/index.js
@@ -1,11 +1,13 @@
+import ImputLabel from "@/components/CustomImputLabel";
 import Subtitulo from "@/components/Subtitulo";
-import Titulo from "@/components/titulo";
+import Titulo from "@/components/Titulo";
 
 export default function Home() {
   return (  
     <div>
       <Titulo></Titulo>
-      <Subtitulo texto="Estou Aqui!"></Subtitulo> 
+      <Subtitulo texto="Estou Aqui!"></Subtitulo>
+      <ImputLabel></ImputLabel>
     </div>
 
   );
