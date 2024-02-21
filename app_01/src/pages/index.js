2f2a9ab59e4fa886e9145a86eeb0ce8acbe295ec
DM0k <129907524+DM0k@users.noreply.github.com>
2024-02-20 21:41:23 -0300

    Mais componentes personalizados

--- app_01/src/pages/index.js
+++ app_01/src/pages/index.js
@@ -1,13 +1,22 @@
-import ImputLabel from "@/components/CustomImputLabel";
+import CustomCheckBox from "@/components/CustomCheckBox";
+import CustomImputLabel from "@/components/CustomImputLabel";
+import CustomTextAreaLabel from "@/components/CustomTextAreaLabel";
 import Subtitulo from "@/components/Subtitulo";
 import Titulo from "@/components/Titulo";
 
 export default function Home() {
   return (  
-    <div>
+    <div className="container-fluid">
       <Titulo></Titulo>
       <Subtitulo texto="Estou Aqui!"></Subtitulo>
-      <ImputLabel></ImputLabel>
+      <div className="row">
+      <CustomImputLabel id="txtNome" texto="Nome" col="8"/>
+      <CustomImputLabel id="txtPreco" texto="Preço" col="4"/>
+      <CustomTextAreaLabel id="txtDescrição" texto="Descrição" col="12"/>
+      <CustomImputLabel id="txtImagem" texto="Imagem" col="12"/>
+      <CustomCheckBox id="txtDisponivel" texto="Disponível" col="6"/>
+      <CustomCheckBox id="txtNovidade" texto="Novidade" col="6"/>
+      </div>
     </div>
 
   );
